a7ae642c7a1e22ae0861db6e137d8e324db7b084
Roman Borysenko <romanborysenko2702@gmail.com>
2024-12-10 02:32:16 +0200

    fix bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,9 +16,10 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = Object.fromEntries(formData.entries());
   const { customerId, amount, status } = CreateInvoice.parse({
-    rawFormData,
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
